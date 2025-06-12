74ab1e790ca828d393aebdb775d179db9d5fce22
ThirumaranM <thirumaran.m@meyicloud.com>
2025-06-12 15:54:06 +0530

    added changes

--- new-portfolio/.eslintrc.cjs
+++ new-portfolio/.eslintrc.cjs
@@ -1,20 +1,52 @@
+// module.exports = {
+// 	root: true,
+// 	parser: '@typescript-eslint/parser',
+// 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+// 	plugins: ['svelte3', '@typescript-eslint'],
+// 	ignorePatterns: ['*.cjs'],
+// 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+// 	settings: {
+// 		'svelte3/typescript': () => require('typescript')
+// 	},
+// 	parserOptions: {
+// 		sourceType: 'module',
+// 		ecmaVersion: 2020
+// 	},
+// 	env: {
+// 		browser: true,
+// 		es2017: true,
+// 		node: true
+// 	}
+// };
+
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
+	plugins: ['svelte', '@typescript-eslint'],
+	extends: [
+	  'eslint:recommended',
+	  'plugin:@typescript-eslint/recommended',
+	  'plugin:svelte/recommended',
+	  'prettier'
+	],
+	overrides: [
+	  {
+		files: ['*.svelte'],
+		parser: 'svelte-eslint-parser',
+		parserOptions: {
+		  parser: {
+			ts: '@typescript-eslint/parser',
+		  },
+		  extraFileExtensions: ['.svelte'],
+		  ecmaVersion: 2020,
+		  sourceType: 'module',
+		},
+	  }
+	],
 	env: {
-		browser: true,
-		es2017: true,
-		node: true
+	  browser: true,
+	  es2017: true,
+	  node: true,
 	}
-};
+  };
+  
--- new-portfolio/svelte.config.js
+++ new-portfolio/svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
-
+// const base = '/slick-portfolio-svelte';
+const base = '';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
